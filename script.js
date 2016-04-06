e619930c3fcdbfa24dac18d44f70321979faebe3
Leah Watt <leahpwatt@gmail.com>
2016-04-06 17:51:35 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*alert ("Welcome to my site!")
+
+var name = prompt('Please enter your name'); alert('Thanks' + ' ' + name + '!');
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("Name must be filled out");
+        return false;
+    }*/
+
+function popup() {
+    alert("Thanks for your submission!")
+}
+
+function prompter() { 
+	var reply = prompt("Welcome to my site! What's your name?")
+	alert ("Thanks for stopping by " + reply + "!")
+}
