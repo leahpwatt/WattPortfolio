0c8243c711f4e716440d3b17d04d1d0b9039dda0
Leah <leahpwatt@gmail.com>
2016-04-11 16:51:05 -0400

    changes to html, css, js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-/*alert ("Welcome to my site!")
-
-var name = prompt('Please enter your name'); alert('Thanks' + ' ' + name + '!');
-
-
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == null || x == "") {
-        alert("Name must be filled out");
-        return false;
-    }*/
-
 function popup() {
     alert("Thanks for your submission!")
 }
